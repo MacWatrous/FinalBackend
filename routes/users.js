a87d1c280e241bf5876504e1e48a83ce59e95769
macwatrous <watrm589@newschool.edu>
2016-12-03 16:08:20 -0500

    testing db

--- routes/users.js
+++ routes/users.js
@@ -18,12 +18,11 @@ router.get('/:id', function(req, res, next) {
     var id = req.params.id;
     connection.connect();
     connection.query('SHOW VARIABLES LIKE "%version%";', function(err,rows,fields){
-        console.log(dbURL);
         console.log(id);
         console.log(rows[1].Variable_name);
     });
     connection.end();
-    res.send(id + " : " + dbURL);
+    res.send(id);
     next();
 });
 
