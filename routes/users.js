3cc1f728c1862e1ebe215442297c800fa75ba1e5
macwatrous <watrm589@newschool.edu>
2016-12-06 11:45:14 -0500

    adding post functionality /users/{id}

--- routes/users.js
+++ routes/users.js
@@ -35,4 +35,29 @@ router.get('/:id', function(req, res, next) {
     //next();
 });
 
+router.post('/:id', function(req, res, next) {
+    var id = req.params.id;
+    var body = req.body.cash;
+    //console.log(req.app.models);
+    req.app.models.user.findOneById(id).exec(function (err, find){
+        if (err) {
+            res.status(500).json({error: 'Error when trying to find user.'});
+        }
+        else if (!find) {
+            res.status(401).json({error: "User does not exist"});
+        }
+        else {
+            //found user
+            var result = {
+                id: id,
+                username: find.username,
+                cash: find.cash
+            };
+            res.json(result);
+        }
+    });
+    //res.send(id);
+    //next();
+});
+
 module.exports = router;
