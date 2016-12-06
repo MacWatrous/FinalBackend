9a99bff323d37fb0f26936c836c7f3a14c3980f4
macwatrous <watrm589@newschool.edu>
2016-12-06 01:17:21 -0500

    creating /users/portfolio/{id}

--- routes/userPortfolio.js
+++ routes/userPortfolio.js
@@ -45,4 +45,42 @@ router.get('/:id', function(req, res, next) {
     //next();
 });
 
+router.get('/:id/:ticker', function(req, res, next) {
+    var id = req.params.id;
+    var ticker = req.params.ticker;
+    //console.log(req.app.models);
+
+    req.app.models.stocks.findById(id).exec(function (err, find){
+        if (err) {
+            res.status(500).json({error: 'Error when trying to find a users stocks'});
+        }
+        else if (!find) {
+            res.status(401).json({error: "User has no stocks"});
+        }
+        else {
+            //found user
+            var stocks = [];
+            for (var i =0;i<find.length;i++){
+                if (find[i].stockTicker == ticker) {
+                    var indvStock = {
+                        ticker: find[i].stockTicker,
+                        purchaseDate: find[i].purchaseDate,
+                        sharePrice: find[i].purchasePrice,
+                        shares: find[i].purchaseAmount
+                    };
+                    stocks.push(indvStock)
+                }
+            }
+
+            var result = {
+                id: id,
+                stocks: stocks
+            };
+            res.json(result);
+        }
+    });
+    //res.send(id);
+    //next();
+});
+
 module.exports = router;
