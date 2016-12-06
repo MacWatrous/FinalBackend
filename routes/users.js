f0faf55305743c7d4b882600c2ef5e5c5be1f528
macwatrous <watrm589@newschool.edu>
2016-12-05 23:39:19 -0500

    dependency update

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,7 @@ router.get('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
     var id = req.params.id;
     console.log(id);
-    console.log(app.models);
-
+    console.log(req.app.models);
     app.models.user.findOneById(id).exec(function (err, find){
         if (err) {
             res.status(500).json({error: 'Error when trying to find user.'});
