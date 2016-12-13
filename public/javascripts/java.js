bc1eac8d2862b72f7c005893e1648b6f17dfadb2
macwatrous <watrm589@newschool.edu>
2016-12-12 22:57:16 -0500

    new chart

--- public/javascripts/java.js
+++ public/javascripts/java.js
@@ -7,162 +7,7 @@ var global_stockarray;
 var searchFor;
 var runCount = 0;
 var stockLoop;
-
-$(function () {
-
-
-    var colors = Highcharts.getOptions().colors,
-        categories = ['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera'],
-        data = [{
-            y: 56.33,
-            color: colors[0],
-            drilldown: {
-                name: 'MSIE versions',
-                categories: ['MSIE 6.0', 'MSIE 7.0', 'MSIE 8.0', 'MSIE 9.0', 'MSIE 10.0', 'MSIE 11.0'],
-                data: [1.06, 0.5, 17.2, 8.11, 5.33, 24.13],
-                color: colors[0]
-            }
-        }, {
-            y: 10.38,
-            color: colors[1],
-            drilldown: {
-                name: 'Firefox versions',
-                categories: ['Firefox v31', 'Firefox v32', 'Firefox v33', 'Firefox v35', 'Firefox v36', 'Firefox v37', 'Firefox v38'],
-                data: [0.33, 0.15, 0.22, 1.27, 2.76, 2.32, 2.31, 1.02],
-                color: colors[1]
-            }
-        }, {
-            y: 24.03,
-            color: colors[2],
-            drilldown: {
-                name: 'Chrome versions',
-                categories: ['Chrome v30.0', 'Chrome v31.0', 'Chrome v32.0', 'Chrome v33.0', 'Chrome v34.0',
-                    'Chrome v35.0', 'Chrome v36.0', 'Chrome v37.0', 'Chrome v38.0', 'Chrome v39.0', 'Chrome v40.0', 'Chrome v41.0', 'Chrome v42.0', 'Chrome v43.0'
-                    ],
-                data: [0.14, 1.24, 0.55, 0.19, 0.14, 0.85, 2.53, 0.38, 0.6, 2.96, 5, 4.32, 3.68, 1.45],
-                color: colors[2]
-            }
-        }, {
-            y: 4.77,
-            color: colors[3],
-            drilldown: {
-                name: 'Safari versions',
-                categories: ['Safari v5.0', 'Safari v5.1', 'Safari v6.1', 'Safari v6.2', 'Safari v7.0', 'Safari v7.1', 'Safari v8.0'],
-                data: [0.3, 0.42, 0.29, 0.17, 0.26, 0.77, 2.56],
-                color: colors[3]
-            }
-        }, {
-            y: 0.91,
-            color: colors[4],
-            drilldown: {
-                name: 'Opera versions',
-                categories: ['Opera v12.x', 'Opera v27', 'Opera v28', 'Opera v29'],
-                data: [0.34, 0.17, 0.24, 0.16],
-                color: colors[4]
-            }
-        }, {
-            y: 0.2,
-            color: colors[5],
-            drilldown: {
-                name: 'Proprietary or Undetectable',
-                categories: [],
-                data: [],
-                color: colors[5]
-            }
-        }],
-        browserData = [],
-        versionsData = [],
-        i,
-        j,
-        dataLen = data.length,
-        drillDataLen,
-        brightness;
-
-
-    // Build the data arrays
-    for (i = 0; i < dataLen; i += 1) {
-
-        // add browser data
-        browserData.push({
-            name: categories[i],
-            y: data[i].y,
-            color: data[i].color
-        });
-
-        // add version data
-        drillDataLen = data[i].drilldown.data.length;
-        for (j = 0; j < drillDataLen; j += 1) {
-            brightness = 0.2 - (j / drillDataLen) / 5;
-            versionsData.push({
-                name: data[i].drilldown.categories[j],
-                y: data[i].drilldown.data[j],
-                color: Highcharts.Color(data[i].color).brighten(brightness).get()
-            });
-        }
-    }
-
-    // Create the chart
-    var chart = Highcharts.chart("pie", {
-        chart: {
-            type: 'pie',
-            renderTo: 'container',
-            marginTop: -50,
-            marginBottom: -50,
-            marginLeft: 0,
-            marginRight: 0
-        },
-        title: {
-            text: null
-        },
-        yAxis: {
-            title: {
-                text: 'Total percent market share'
-            }
-        },
-        credits: false,
-        plotOptions: {
-            pie: {
-                shadow: false,
-                center: ['50%', '50%'],
-                size: '100%'
-            }
-        },
-        tooltip: {
-            valueSuffix: '%'
-        },
-        series: [{
-            name: 'Browsers',
-            data: browserData,
-            size: '60%',
-            dataLabels: {
-                formatter: function () {
-                    return this.y > 5 ? this.point.name : null;
-                },
-                color: '#ffffff',
-                distance: -30
-            }
-            
-        }, {
-            name: 'Versions',
-            data: versionsData,
-            size: '80%',
-            innerSize: '60%',
-            dataLabels: {
-                formatter: function () {
-                    // display only if larger than 1
-                    return this.y > 1 ? '<b>' + this.point.name + ':</b> ' + this.y + '%' : null;
-                }
-            }
-        }]
-        
-    });
-
-    var opt = chart.series[1].options;
-    opt.dataLabels.enabled = !opt.dataLabels.enabled;
-    chart.series[1].update(opt);
-});
-
-
+var mainTable;
 
 $('#searchbtn').on('click', searchDialog);
 function searchDialog(event) {
@@ -175,7 +20,9 @@ function searchDialog(event) {
 }
 $('#addbtn').on('click', function(e) {
     e.preventDefault();
+
     clearInterval(stockLoop);
+    mainGraph();
     $('#overlayhider').hide();
 });
 
@@ -221,6 +68,7 @@ function mainLoop() {
         contentType: 'application/json; charset=UTF-8',
         url: '/stocks'
     }).done(function(response) {
+        mainTable = response;
         $('#companyname').empty();
         $('#price').empty();
         $('#shares').empty();
@@ -277,6 +125,7 @@ function googleLoop() {
         if (runCount == 0){
             $(function(){
                 $("ul#ticker01").liScroll();
+                mainGraph();
             });
         }
         runCount++;
@@ -334,6 +183,41 @@ function makeGraph(labels, data){
     });
 }
 
+var ctx2 = document.getElementById("myChartPie");
+
+function mainGraph(){
+    var backgroundColor = [];
+    var borderColor = [];
+    var labels = [];
+    var data = [];
+    for (var i = 0;i<mainTable.length; i++){
+        labels.push(mainTable[i].company);
+        data.push(mainTable[i].positionVal);
+        backgroundColor.push('rgba('+100+','+i+40+','+i+25+',0.7'+')');
+        borderColor.push('rgba('+100+','+i+40+','+i+25+',1'+')');
+    }
+    var myChartPie = new Chart(ctx2, {
+        type: 'polarArea',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Position Val ($)',
+                data: data,
+                backgroundColor: backgroundColor,
+                borderColor: borderColor,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            startAngle: -.70,
+            //maintainAspectRatio: true,
+            scales: {
+                display: false
+            }
+        }
+    });
+}
+
 function overlayLoop(){
     $.ajax({
         method: 'GET',
@@ -371,7 +255,11 @@ function overlayLoop(){
         $("#200d").empty();
         $("#change").empty();
         $("#exchange").empty();
+        $("#name").empty();
+        $("#ticker").empty();
 
+        $("#name").append($('<h1></h1>').text(response.company));
+        $("#ticker").append($('<h4></h4>').text(response.stockTicker));
         $("#52wk-L").append($('<p></p>').text('$'+response.weekLow52));
         $("#52wk-H").append($('<p></p>').text('$'+response.weekHigh52));
         $("#mktcap").append($('<p></p>').text('$'+response.marketCap));
